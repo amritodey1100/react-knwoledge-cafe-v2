17b8c39acf70e3bea1aa0fba005effc23eee2a21
Amrito Dey <amritodey07@gmail.com>
2025-02-28 11:44:37 +0600

    added more basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
   return (
     <>
       <Header />
-      <Blogs />
+      <header className='md:flex'>
+        <Blogs />
+        <Bookmarks />
+      </header>
     </>
   )
 }
---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bookmarks = () => {
+    return (
+        <div className="md:w-1/3">
+            <h1 className="text-4xl">Bookmarks</h1>
+            {/* Add bookmark rendering logic here */}
+        </div>
+    );
+};
+
+export default Bookmarks;
