70b84eda2f5496e5e7653c2714a112ae64c258aa
Amrito Dey <amritodey07@gmail.com>
2025-02-28 15:18:41 +0600

    basic-structure-complete

--- src/components/Blogs/blogs.jsx
+++ src/components/Blogs/blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Blog from "../blog/blog";
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -12,7 +13,9 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
-             
+             {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+             }
         </div>
     );
 };
--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,10 +1,34 @@
+
+import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover, reading_time, author, author_img, posted_date, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover Picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex items-center'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-4'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} Min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hashtag, index) => (
+                        <span key={index} className='mr-2'>#{hashtag}</span>
+                    ))
+                }
+            </p>
         </div>
     );
 };
-
+Blog.proptypes = {
+    blog: PropTypes.object.isRequired
+}
 export default Blog;
