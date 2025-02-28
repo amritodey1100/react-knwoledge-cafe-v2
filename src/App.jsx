468bea3b34c6131cb2f8a95c5b5de0e658ccf854
Amrito Dey <amritodey07@gmail.com>
2025-02-28 11:01:00 +0600

    basic-complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Blogs />
+    </>
+  )
+}
+
+export default App
